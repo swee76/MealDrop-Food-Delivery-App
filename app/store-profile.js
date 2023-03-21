1c7d74882d46500654df8c20815f78d9ec328573
Swee76 <oshinikithma1213@gmail.com>
2023-03-22 00:53:11 +0530

    create store profile for manage store details

---
+++ app/store-profile.js
@@ -0,0 +1,49 @@
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import {Link} from "expo-router";
+import BasicPageWrapper from "../components/wrappers/BasicPageWrapper";
+import StoreDetailsForm from "../components/forms/StoreDetailsForm";
+
+const StoreProfile = () => {
+    return (
+        <BasicPageWrapper>
+            <View>
+                <View style={styles.breadcrumbContainer}>
+                    <Link href={'/'} style={styles.goBack}>Go Back</Link>
+                    <TouchableOpacity style={styles.menuItemsButton}>
+                        <Text style={styles.menuItemsButtonText}>
+                            Handle Menu Items
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <StoreDetailsForm/>
+            </View>
+        </BasicPageWrapper>
+    )
+}
+
+export default StoreProfile;
+
+const styles = StyleSheet.create({
+    breadcrumbContainer: {
+        display: "flex",
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 2,
+        margin: 18
+    },
+    goBack: {
+        width: '15%'
+    },
+    menuItemsButton: {
+        width: '30%',
+        backgroundColor: '#d0a852',
+        borderRadius: 16,
+    },
+    menuItemsButtonText: {
+        paddingVertical: 8,
+        paddingHorizontal: 6,
+        textAlign: 'center',
+        fontWeight: '600',
+        fontSize: 10,
+    }
+})
