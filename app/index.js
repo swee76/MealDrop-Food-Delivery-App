b57b483416cb319b11916bb89ae5a7e41a0a2c24
Osini Kithma <66475844+swee76@users.noreply.github.com>
2023-03-22 22:51:06 +0530

    conflict fixed

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import {Link} from "expo-router";
 
 const HomePage = () => (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello World</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'} }>
+        <Text >Hello World</Text>
         <Link href={'/'}>Home</Link>
         <Link href={'/profile'}>Profile</Link>
         <Link href={'/store-profile'}>Manage Store Details</Link>
