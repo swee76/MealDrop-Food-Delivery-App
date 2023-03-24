d1797734546c6b70c7954561403d7be4c7a0744a
kethmi <it20021320@my.sliit.lk>
2023-03-24 20:28:41 +0530

    food cart

--- app/index.js
+++ app/index.js
@@ -1,34 +1,18 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 import {Link} from "expo-router";
+import Admin from './menuScreen';
 
 const HomePage = () => (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={styles.header}>Navigation Links for Sections</Text>
-        <View style={styles.navLinks}>
-            <Link href={'/'}>Home</Link>
-            <Link href={'/profile'}>Profile</Link>
-            <Link href={'/store-profile'}>Manage Store Details</Link>
-            <Link href={'/login'}>Login</Link>
-            <Link href={'/choose-type'}>Profile Type select</Link>
-            <Link href={'/adminhome'}>Admin Home</Link>
-            <Link href={'/food-menu'}>View Food Menu List</Link>
-            <Link href={'/view-store-list'}>View Store List</Link>
-        </View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'} }>
+        <Text>Hello World</Text>
+        <Link href={'/'}>Home</Link>
+        <Link href={'/profile'}>Profile</Link>
+        <Link href={'/store-profile'}>Manage Store Details</Link>
+        <Link href={'/login'}>Login</Link>
+        <Link href={'/choose-type'}>Profile Type select</Link>
+        <Link href={'/AdminHome'}>Admin Home</Link>
+            <Link href={'/customer-profile'}>Customer Details</Link>
     </View>
 );
 
 export default HomePage;
-
-const styles = StyleSheet.create({
-    header: {
-        fontSize: 20,
-        fontWeight: '600',
-        textDecorationLine: 'underline'
-    },
-    navLinks: {
-        marginTop: 14,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        gap: 10
-    }
-});
