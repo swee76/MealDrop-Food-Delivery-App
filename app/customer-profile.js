b17149550d52576b3b5570c7be8a56457e1be77d
kethmi <it20021320@my.sliit.lk>
2023-03-24 20:28:41 +0530

    food cart

--- app/customer-profile.js
+++ app/customer-profile.js
@@ -2,21 +2,17 @@ import {Text, TouchableOpacity, View, StyleSheet, Image} from 'react-native';
 import { Link, useRouter } from "expo-router";
 import BasicPageWrapper from "../components/wrappers/BasicPageWrapper";
 import React, {useEffect, useState} from "react";
-import {ref, onValue, update} from "firebase/database";
+import { ref, onValue} from "firebase/database";
 import {database} from "../firebase";
 import {getObject} from "../storage";
 
 const CustomerProfile = () => {
 
-    const [name, setName] = useState(null);
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [Phone, setPhone] = useState(null);
-    const [Address, setAddress] = useState(null);
-    const [City, setCity] = useState(null);
-    const [user, setUser] = useState(null);
-    const [trigger, setTrigger] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [userId, setUserId] = useState('')
+    const [Phone, setPhone] = useState('');
+    const [Address, setAddress] = useState('');
+    const [City, setCity] = useState('');
 
     const router = useRouter();
 
@@ -24,18 +20,15 @@ const CustomerProfile = () => {
         getObject('user').then((data)=>{
             readUser(data.id).then((user)=>{
                 console.log(user)
+              //  setName(user.name)
                 setEmail(user.email)
-                setName(user.name)
-                setPhone(user.Phone)
-                setAddress(user.Address)
-                setCity(user.City)
             })
         })
         //readUser()
     },[]);
 
     const readUser = async (userId) => {
-        const userData = ref(database, 'users/' + userId,userData);
+        const userData = ref(database, 'users/' + userId);
         let user;
         onValue(userData, (snapshot) => {
             user = snapshot.val();
@@ -43,44 +36,12 @@ const CustomerProfile = () => {
         return user;
     };
 
-
-    useEffect(() => {
-        getObject('user')
-            .then((data) => {
-                setUserId(data.id)
-                readUser(data.id)
-                    .then((user) => {
-                        setUser(user)
-                    })
-            })
-    }, [])
-    const deleteCustomer = async () => {
-        setLoading(true)
-
-        const userData = {
-            name: null,
-            Phone: null,
-            City: null,
-            Address:null
-
-        }
-
-
-        update(ref(database, 'users/' + userId), userData)
-            .then(() => {
-                setTrigger(!trigger)
-                setLoading(false)
-                router.push('/update-customer')
-            })
-        alert("User Information Deleted Successfully!!");
-    }
-
     return (
         <BasicPageWrapper>
             <View style={styles.container}>
                 <Text style={styles.heading}>User Details</Text>
-                <Image source={require('../assets/eat.png')}
-                       style={{width: 180, height: 140, margin: 12}}/>
+                <Image source={require('../assets/landing-home-phone-new.png')}
+                       style={{width: 200, height: 160, margin: 12}}/>
                 <View style={styles.row}>
                     <Text style={styles.label} >Name:</Text>
                     <Text style={styles.value} >{name}</Text>
@@ -91,24 +52,21 @@ const CustomerProfile = () => {
                 </View>
                 <View style={styles.row}>
                     <Text style={styles.label}>Phone:</Text>
-                    <Text style={styles.value}>{Phone}</Text>
+                    <Text style={styles.value}>(123) 456-7890</Text>
                 </View>
                 <View style={styles.row}>
                     <Text style={styles.label}>Address:</Text>
-                    <Text style={styles.value}>{Address}</Text>
+                    <Text style={styles.value}>123 Main St.</Text>
                 </View>
                 <View style={styles.row}>
                     <Text style={styles.label}>City:</Text>
-                    <Text style={styles.value}>{City}</Text>
+                    <Text style={styles.value}>Anytown</Text>
                 </View>
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity style={[styles.button, styles.editButton]} onPress={() => {
                         router.push('/update-customer')
                     }}>
                         <Text style={styles.buttonText}>Edit</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, styles.editButton]} onPress={deleteCustomer} >
-                        <Text style={styles.buttonText} >Delete</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.button} onPress={() => {
                         router.push('/')
