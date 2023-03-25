3ed3a1cc0e0a5918478b30002f11eb085930b076
Swee76 <oshinikithma1213@gmail.com>
2023-03-25 11:36:44 +0530

    styles bug fixes

--- components/forms/StoreDetailsForm.jsx
+++ components/forms/StoreDetailsForm.jsx
@@ -104,6 +104,7 @@ const styles = StyleSheet.create({
             justifyContent: 'center',
             marginVertical: 16,
             position: 'relative',
+            zIndex:-1
         },
         heading: {
             fontSize: 24,
