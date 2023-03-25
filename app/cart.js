5c536320c000eb3264c995f2e1f5d0649545dcf9
kethmi <it20021320@my.sliit.lk>
2023-03-25 11:39:01 +0530

    search menu features

--- app/cart.js
+++ app/cart.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import {View, Text, Button, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import BasicPageWrapper from "../components/wrappers/BasicPageWrapper";
+import {useRouter} from "expo-router";
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Cart = () => {
     const [price, setPrice] = useState(10);
     const [quantity, setQuantity] = useState(1);
     const [cart, setCart] = useState([]);
+    const router = useRouter();
 
     const addToCart = () => {
         const item = { price: price * quantity };
@@ -21,20 +24,39 @@ const Cart = () => {
 
     return (
         <BasicPageWrapper>
-            <View style={styles.container}>
+            <View>
                 <Text style={styles.title}>Food Item</Text>
-                <Text style={styles.price}>${price.toFixed(2)}</Text>
-                <View style={styles.quantityContainer}>
-                    <Button title="-" onPress={() => setQuantity(Math.max(quantity - 1, 1))} />
-                    <Text style={styles.quantity}>{quantity}</Text>
-                    <Button title="+" onPress={() => setQuantity(quantity + 1)} />
+                <Image source={require('../assets/cart.png')}
+                       style={{width: 250, height: 200, margin: 20, marginLeft:20,resizeMode: 'contain'}}/>
+                <View style={styles.btnm}><Text style={styles.price}>${price.toFixed(2)}</Text>
+                    <View style={styles.quantityContainer}>
+                        <TouchableOpacity onPress={() => setQuantity(Math.max(quantity - 1,1))}>
+                            <MaterialIcons name="remove" size={24} color="black"/>
+                        </TouchableOpacity>
+                        <Text style={styles.quantity}>{quantity}</Text>
+
+
+                        <TouchableOpacity onPress={() => setQuantity(quantity + 1)}>
+                            <MaterialIcons name="add" size={24} color="black"/>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <Button title="Add to Cart" onPress={addToCart} />
-                <Text style={styles.cartTitle}>Cart</Text>
+
+                    <TouchableOpacity style={[styles.buttonx, styles.editButton]} onPress={addToCart}>
+                        <Text style={styles.buttonText}>Add Cart</Text>
+                    </TouchableOpacity>
+                <Text style={styles.cartTitle}>Cart Items</Text>
                 {cart.map((item, index) => (
                     <Text key={index} style={styles.cartItem}>${item.price.toFixed(2)}</Text>
                 ))}
-                <Button title="Clear Cart" onPress={clearCart} />
+                <View style={styles.btn}><TouchableOpacity style={[styles.button, styles.editButton]} onPress={clearCart}>
+                    <Text style={styles.buttonText}>Clear Cart</Text>
+                </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => {
+                        router.push('/')
+                    }}>
+                        <Text style={styles.buttonText}>Back</Text>
+                    </TouchableOpacity></View>
             </View>
         </BasicPageWrapper>
     );
@@ -75,6 +97,45 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginBottom: 5,
     },
+    button: {
+        backgroundColor: '#FF5A5F',
+        borderRadius: 4,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        minWidth: 100,
+    },
+    buttonx: {
+        backgroundColor: '#FF5A5F',
+        borderRadius: 4,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        minWidth: 100,
+        marginLeft:20
+    },
+    editButton: {
+        backgroundColor: '#d0a852',
+        marginRight: 20,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    btn:{
+        flexDirection:'row',
+        marginLeft:70
+
+    },
+    btnm:{
+
+        flexDirection:'row',
+
+    },
+    new:{
+        justifyContent:'space-evenly',
+    }
+
 });
 
 export default Cart;
