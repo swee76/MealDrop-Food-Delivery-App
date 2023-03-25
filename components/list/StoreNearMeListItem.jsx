d5cd3fc6fc90b48aea677c7147359565f0433c61
Flashminat0 <wyasaslowe@gmail.com>
2023-03-25 12:44:16 +0530

    image uplaod blob fix

--- components/list/StoreNearMeListItem.jsx
+++ components/list/StoreNearMeListItem.jsx
@@ -1,11 +1,31 @@
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 
 const StoreNearMeListItem = ({store}) => {
     return (
-        <View>
-            <Text>{store.storeName}</Text>
+        <View style={styles.row}>
+            <Text style={styles.title}>{store.storeName}</Text>
+            <Text style={styles.title}>{store.contactInfo}</Text>
         </View>
     );
 };
 
 export default StoreNearMeListItem;
+
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray',
+    },
+    title: {
+        fontSize: 18,
+    },
+    subTitle: {
+        color: 'gray',
+
+    }
+})
