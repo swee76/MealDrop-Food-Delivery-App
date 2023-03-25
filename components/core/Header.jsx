78995cfa4064ef24040f5dba9512e93d939798ff
Flashminat0 <wyasaslowe@gmail.com>
2023-03-25 10:07:19 +0530

    list with stores

--- components/core/Header.jsx
+++ components/core/Header.jsx
@@ -1,11 +1,7 @@
 import {Text, View} from "react-native";
-import {useEffect} from "react";
 
 
 const Header = ({user}) => {
-    useEffect(() => {
-        console.log(user)
-    }, [user])
 
     return (
         <View style={{
